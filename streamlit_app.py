f268bbcabe187d3b20ebd859d765ff4abe54951a
snowflake-throwaway <snowflake-throwaway@alanellis.co.uk>
2025-03-18 09:48:46 +0000

    Update streamlit_app.py troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,11 +32,9 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
-    
+if ingredients_list:   
     ingredients_string = ''
+    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
